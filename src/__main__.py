92111b424e4bb11a573a6649f45a13346a676a28
Girard Louis <lgirard@student.42lyon.fr>
2026-05-20 18:27:28 +0200

    save

--- src/__main__.py
+++ src/__main__.py
@@ -6,4 +6,11 @@ if __name__ == "__main__":
     wallet: Wallet = wallet_man.init_new_wallet()
 
     print(wallet.phrase)
-    print(f"BTC Price: {wallet_man.get_bitcoin_price()}")
+
+    btc_price = wallet_man.get_bitcoin_price()
+    usd_price = btc_price.get("usd")
+    eur_price = btc_price.get("eur")
+
+    print(f"Balance: {wallet_man.get_balance(wallet) * usd_price}$")
+
+    print(f"BTC Price: {usd_price}$ | {eur_price}€")
